408fb18fc3acd355944e78cb63642393e33322cd
MartinezFabian <FabianMartinezDev@gmail.com>
2024-06-06 21:00:28 -0300

    Add conditional rendering based on isLoading and errorMessage in Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 
 import EmployeeTable from './components/EmployeeTable.tsx';
-import { Grid, Typography } from '@mui/material';
+import { Alert, CircularProgress, Grid, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux/store/store.ts';
 
 interface HomeProps {
   // define your props here
 }
 
 const Home: React.FC<HomeProps> = () => {
+  const { isLoading, errorMessage } = useSelector((state: RootState) => state.employees);
+
   return (
     <>
       <Typography variant="h5" component="h1" sx={{ paddingLeft: 0, paddingBottom: 3 }}>
         Lista de Empleados
       </Typography>
       <Grid container gap={4}>
-        <EmployeeTable />
+        {isLoading ? (
+          <CircularProgress sx={{ margin: 'auto' }} color="primary" />
+        ) : errorMessage.length > 0 ? (
+          <Alert severity="error">{errorMessage}</Alert>
+        ) : (
+          <EmployeeTable />
+        )}
       </Grid>
     </>
   );
